9c3f3df6e3531e5697cbb5963eb8c7d1ef97b33f
Miro <badyss.blt@gmail.com>
2023-09-02 18:04:32 +0200

    slider

--- src/components/slider.php
+++ src/components/slider.php
@@ -4,19 +4,25 @@
     $products = $query->getAllProduct();
 ?>
 <div class="slider">
+    <div class="slider__left">
+        <button onclick="previous()"  class="btn"><i class="fa-solid fa-arrow-left"></i></button>
+    </div>
     <div class="slider__content">
+        
     <?php foreach($products as $slide): ?>
         <div class="slide">
             <h1 style="font-family: 'Merriweather'; color: white; font-size: 3em; letter-spacing: 3px; font-weight: 100;"><?= $slide["name"] ?></h1>
+            <div class="imageContainer">
             <img src="data:image/jpeg;base64,<?= base64_encode($slide['image'])?>"/>
-            <h3 style="font-family: 'Alegreya'; color: white; font-size: 1em;" ><?= $slide["price"]?> €</h3>
+            </div>
+            <h3 style="font-family: 'Roboto'; color: white; font-size: 1em; font-size: 1.3em" ><?= $slide["price"]?> €</h3>
             <a href="#" class="link">Acheter maintenant</a>
         </div>
     
     <?php endforeach ?>
     </div>
-    <div>
-        <button onclick="previous()"  class="btn"><i class="fa-solid fa-arrow-left"></i></button>
+    <div class="slider__right">
         <button onclick="next()" class="btn"><i class="fa-solid fa-arrow-right"></i></button>
     </div>
+    
 </div>
